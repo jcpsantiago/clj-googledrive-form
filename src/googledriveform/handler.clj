e5e7a529c6c768956a5d1695b838d5931168cdf9
Santiago <santiago@billie.io>
2019-08-28 17:33:18 +0200

    merge form develop

--- src/googledriveform/handler.clj
+++ src/googledriveform/handler.clj
@@ -88,85 +88,87 @@
                               "/css/style.css")
                 (include-js "https://kit.fontawesome.com/8a9f12dee1.js"
                             "https://code.jquery.com/jquery-3.4.1.min.js")
-          [:title "The Google Drive void"]]
-     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
 
-    [:body
-     [:div {:class "w-100 fixed bg-yellow"}
-      [:div {:class "w-70 center"}
-       [:h1 {:class "f2 fl athelas i fw2"}
-        [:a {:class "link black" :href "/"} "pepe."]]
-       [:h1 {:class "f2 fr fw2 tracking-mega"}
-        [:a {:href "https://github.com/jcpsantiago"}
-         [:p {:class "dn"} "hidden :D"] ;; helps with alignment
-         [:i {:class "black-20 hover-black fab fa-github"}]]
-        [:a {:href "https://www.linkedin.com/in/jcpsantiago/"}
-         [:i {:class "black-20 hover-black fab fa-linkedin pl2"}]]]]]
+          [:title "The Google Drive void"]
+          [:meta {:charset "utf-8"}]
+          [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]]
+
+         [:body
+          [:div {:class "w-100 fixed bg-yellow"}
+           [:div {:class "w-70 center"}
+            [:h1 {:class "f2 fl athelas i fw2"}
+             [:a {:class "link black" :href "/"} "pepe."]]
+            [:h1 {:class "f2 fr fw2 tracking-mega"}
+             [:a {:href "https://github.com/jcpsantiago"}
+              [:p {:class "dn"} "hidden :D"] ;; helps with alignment
+              [:i {:class "black-20 hover-black fab fa-github"}]]
+             [:a {:href "https://www.linkedin.com/in/jcpsantiago/"}
+              [:i {:class "black-20 hover-black fab fa-linkedin pl2"}]]]]]
 
 
-     [:header {:class "w-100 pa6 ph5-ns bg-yellow"}]
-      ;;[:div {:class "w-30 center"}
-       ;;[:svg {:class "bottom absolute mw-100" :xmlns "http://www.w3.org/2000/svg"}
-        ;;[:circle {:cx "50" :cy "50" :r "50" :fill "black"}]]
+          [:header {:class "w-100 pa6 ph5-ns bg-yellow"}]
+           ;;[:div {:class "w-30 center"}
+            ;;[:svg {:class "bottom absolute mw-100" :xmlns "http://www.w3.org/2000/svg"}
+             ;;[:circle {:cx "50" :cy "50" :r "50" :fill "black"}]]
 
-     [:div {:class "w-60 center"}
-      [:div {:class "w-100 pb4"}
-       [:h2 {:class "f6 athelas ttu trackin fw4 gray mb0 pb0"} "useless clojure things"]
-       [:h1 {:class "f1 athelas tracking lh-title mv0"} "The Google Drive void"]]
+          [:div {:class "w-60 center"}
+           [:div {:class "w-100 pb4"}
+            [:h2 {:class "f6 athelas ttu trackin fw4 gray mb0 pb0"} "useless clojure things"]
+            [:h1 {:class "f1 athelas tracking lh-title mv0"} "The Google Drive void"]]
 
-      [:div {:class (str "lh-copy pb3 measure " inner-viz)}
-       [:p {:class "f4 athelas"}
-           "I lose files in Google Drive all the time. Someone sends me a link,
-            I open it and it's gone forever. Unfortunately, it's a useful service,
-            so I created this form to throw files into the depths. Fill the form
-            below, press send, and rejoice in the fact you'll never find that file again."]]
+           [:div {:class (str "lh-copy pb3 measure " inner-viz)}
+            [:p {:class "f4 athelas"}
+                "I lose files in Google Drive all the time. Someone sends me a link,
+                 I open it and it's gone forever. Unfortunately, it's a useful service,
+                 so I created this form to throw files into the depths. Fill the form
+                 below, press send, and rejoice in the fact you'll never find that file again."]]
 
-      ;; -- form starts here--
-      [:form {:id "uploadform" :class "w-100 measure mb5 avenir"
-              :method "POST" :enctype "multipart/form-data"}
-       (anti-forgery-field)
-       [:div {:class (str "forminner " inner-viz)}
+           ;; -- form starts here--
+           [:form {:id "uploadform" :class "w-100 measure mb5 avenir"
+                   :method "POST" :enctype "multipart/form-data"}
+            (anti-forgery-field)
+            [:div {:class (str "forminner " inner-viz)}
 
-        ;; -- name --
-        [:div {:class "mt2"}
-         [:label {:class "f6 b db mb2" :for "name"} "Your name"]
-         [:input {:id "name" :type "text" :name "name" :required ""
-                  :class input-field-style}]]
+             ;; -- name --
+             [:div {:class "mt2"}
+              [:label {:class "f6 b db mb2" :for "name"} "Your name"]
+              [:input {:id "name" :type "text" :name "name" :required ""
+                       :class input-field-style}]]
 
-        ;; -- random number in european format --
-        [:div {:class "mt3"}
-         [:label {:class "f6 b db mb2" :for "randnumber"} "Random number"]
-         [:input {:id "randnumber" :type "text" :name "randnumber" :required ""
-                  :pattern #"\s*-?((\d{1,3}(\.(\d){3})*)|\d*)(,\d{1,2})?\s?(\u20AC)?\s*"
-                  :class input-field-style}]
-         [:small {:class "f6 black-60 db mb2"} "In european format like 12.345,67"]
+             ;; -- random number in european format --
+             [:div {:class "mt3"}
+              [:label {:class "f6 b db mb2" :for "randnumber"} "Random number"]
+              [:input {:id "randnumber" :type "text" :name "randnumber" :required ""
+                       :pattern #"\s*-?((\d{1,3}(\.(\d){3})*)|\d*)(,\d{1,2})?\s?(\u20AC)?\s*"
+                       :class input-field-style}]
+              [:small {:class "f6 black-60 db mb2"} "In european format like 12.345,67"]
 
-        ;; -- csv input --
-         [:div {:class "mt3"}
-          ;; https://stackoverflow.com/questions/31937112/html-form-cannot-post-file-content
-          [:label {:class "f6 b db mb3" :for "choose-file"} "Your important work spreadsheet"]
+             ;; -- csv input --
+              [:div {:class "mt3"}
+               ;; https://stackoverflow.com/questions/31937112/html-form-cannot-post-file-content
+               [:label {:class "f6 b db mb3" :for "choose-file"} "Your important work spreadsheet"]
 
-          ;; -- https://jsfiddle.net/m7eqj9ku/1/
-          [:span {:class "file-01"}
-           [:input {:id "file-01" :type "file" :name "choose-file" :required ""
-                    :accept ".xls, .xlsx, .ods, .csv, .tsv, .pages,
-                             application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
-                             application/vnd.ms-excel, text/csv,
-                             application/vnd.oasis.opendocument.spreadsheet"
-                    :class "dn w-100 file-01"}]]
-          [:label {:class "b ph3 pv2 ba b--black bg-transparent pointer f6" :for "file-01"}
-           [:span "Upload a file"]]]]
+               ;; -- https://jsfiddle.net/m7eqj9ku/1/
+               [:span {:class "file-01"}
+                [:input {:id "file-01" :type "file" :name "choose-file" :required ""
+                         :accept ".xls, .xlsx, .ods, .csv, .tsv, .pages,
+                                  application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+                                  application/vnd.ms-excel, text/csv,
+                                  application/vnd.oasis.opendocument.spreadsheet"
+                         :class "dn w-100 file-01"}]]
+               [:label {:class "b ph3 pv2 ba b--black bg-transparent pointer f6" :for "file-01"}
+                [:span "Browse..."]]]]
 
-          ;; -- submit button --
-        [:div {:class "mt4"}
-         [:input {:class "b ph3 pv2 input-reset ba b--black white
-                          pointer f6 bg-animate bg-black hover-pink"
-                   :type "submit" :name "lose-file" :value "Lose it forever"}]]]
+               ;; -- submit button --
+             [:div {:class "mt4"}
+              [:button {:class "b ph3 pv2 input-reset ba b--black white
+                               pointer f6 bg-animate bg-black hover-pink"
+                        :type "submit" :name "lose-file" :formnovalidate ""} "Lose it forever"]]]
 
-       ;; -- success page --
-       (success-page success-viz)]]]
+            ;; -- success page --
+            (success-page success-viz)]]]
 
-    ;; -- show the loading "spinner" after clicking send --
-    [:script submit-spinner]
-    ;;[:script upload-button-jq]
-    [:script upload-filename-js]))
+         ;; -- show the loading "spinner" after clicking send --
+         [:script submit-spinner]
+         ;; -- change the text in the "choose data" button to reflect the file --
+         [:script upload-filename-js]))
